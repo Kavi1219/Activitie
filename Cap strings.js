bfd16f9dc0c1f10da597760c9dedc2735a04ccbb
Kavi <86034996+Kavi1219@users.noreply.github.com>
2023-07-02 17:25:50 +0530

    Delete Cap strings.js

--- Cap strings.js
+++
@@ -1,7 +0,0 @@
-
-let str = function () {
-    let arr = "one day ill become the master of programming"
-    let titlecase = arr.toLowerCase().replace(/\b(\w)/g, s => s.toUpperCase());
-    console.log(titlecase)
-  }
-  str();
