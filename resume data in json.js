6e5d89c2b220321519bd8baeafae223012c4f622
brucelee05 <kaviraj1907@gemail.com>
2023-07-04 11:27:23 +0530

    Day5 Q2

---
+++ resume data in json.js
@@ -0,0 +1,43 @@
+let myResume={
+    "Detials": {
+      "name": "KAVIRAJ G",
+      "email": "kavi*****@gamil.com",
+      "phone": "9677******",
+      "degree": "B.E",
+      "location": {
+        "address": "15/16 Sainath Enclave ShenoyNagar",
+        "zipCode": 600030,
+        "city": "Chennai",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+    },
+    "education": [
+      {
+        "institution": "Thiagarajar College Of Engineering",
+        "department": "Mechatronics",
+        "studyType": "fulltime",
+        "batch start year": 2018,
+        "batch end year": 2022,
+        "cgpa": 7.5,
+      }
+
+    ],
+    "skills": [
+      {
+        "name": "FSD",
+        "level": "beginer",
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "script writter",
+      }
+    ]
+  };
+  console.log(myResume);
